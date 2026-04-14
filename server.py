0f5c3520ac126d63545d3af493684a77e88d803e
MedhaBK <medhabk05@gmail.com>
2026-04-14 08:44:28 +0530

    Add home route to Flask server

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 import util
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "House Price Prediction API is LIVE !"
+
+
 @app.route('/get_location_names', methods = ['GET'])
 def get_location_names():
     response = jsonify({
